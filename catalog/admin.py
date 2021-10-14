1097b21450312c4cc6ad0afd198845283cd920f8
Dmitriy1001 <dumatkachuk@gmail.com>
2021-10-14 11:22:43 +0300

    updated admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,10 +1,23 @@
+from django import forms
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Category, Genre, Movie, MovieShot, Actor, Director, Country, RatingStar, Rating, Review
+
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class MovieAdminForm(forms.ModelForm):
+    description = forms.CharField(label='Описание', widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'url')
 
 
 @admin.register(Genre)
@@ -19,7 +32,11 @@ class CountryAdmin(admin.ModelAdmin):
 
 @admin.register(Actor)
 class ActorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'get_photo')
+
+    @admin.display(description='Фото')
+    def get_photo(self, actor):
+        return mark_safe(f'<img src={actor.photo.url} width="70" height="80">')
 
 
 @admin.register(Director)
@@ -27,14 +44,83 @@ class DirectorAdmin(admin.ModelAdmin):
     pass
 
 
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 1
+    readonly_fields = ('name', 'email')
+
+
+class MovieShotInline(admin.TabularInline):
+    model = MovieShot
+    extra = 1
+    readonly_fields = ('get_image',)
+
+    def get_image(self, movie_shot):
+        return mark_safe(f'<img src={movie_shot.image.url} width="120" height="120">')
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'category',
+        'genres',
+        'directors',
+        'year',
+        'countries',
+        'get_poster',
+        'moderation',
+    )
+    list_filter = ('category', 'year')
+    search_fields = ('title', 'description')
     prepopulated_fields = {'url': ('title',)}
+    inlines = (MovieShotInline, ReviewInline)
+    save_on_top = True
+    list_editable = ('moderation',)
+    form = MovieAdminForm
+    actions = ('publish', 'unpublish')
+
+    def unpublish(self, request, queryset):
+        row_update = queryset.update(moderation=True)
+        if row_update == 1:
+            message_bit = '1 запись обновлена'
+        else:
+            message_bit = f''
+        self.message_user(request, message_bit)
+
+    def publish(self, request, queryset):
+        row_update = queryset.update(moderation=False)
+        if row_update == 1:
+            message_bit = '1 запись обновлена'
+        else:
+            message_bit = f'записей обновлено - {row_update}'
+        self.message_user(request, message_bit)
+
+    def get_poster(self, movie):
+        return mark_safe(f'<img src={movie.poster.url} width="70" height="80">')
+
+    genres = lambda self, movie: ', '.join(map(str, movie.genre.all()))
+    directors = lambda self, movie: ', '.join(map(str, movie.director.all()))
+    countries = lambda self, movie: ', '.join(map(str, movie.country.all()))
+
+    unpublish.short_description = 'Опубликовать'
+    publish.short_description = 'Снять с публикации'
+    publish.allowed_permissions = ('change',)
+    unpublish.allowed_permissions = ('change',)
+
+    get_poster.short_description = 'Постер'
+    genres.short_description = 'Жанр'
+    directors.short_description = 'Режисер'
+    countries.short_description = 'Страна'
 
 
 @admin.register(MovieShot)
 class MovieShotAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'get_image')
+
+    @admin.display(description='Кадр')
+    def get_image(self, movie_shot):
+        return mark_safe(f'<img src={movie_shot.image.url} width="120" height="120">')
 
 
 @admin.register(RatingStar)
@@ -49,10 +135,12 @@ class RatingAdmin(admin.ModelAdmin):
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('movie', 'posted')
+    readonly_fields = ('name', 'email')
 
 
-
+admin.site.site_title = 'MovieDom'
+admin.site.site_header = 'MovieDom'
 
 
 
